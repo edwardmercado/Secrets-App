21821679a2459b6ea19cc4e4f1f7d3513ebb4f38
edwardmercado <edwardallenmercado@gmail.com>
2021-01-28 09:46:45 +0800

    Level 2 - Add dotenv to leverage environment variables

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 //jshint esversion:6
+
+require('dotenv').config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -19,8 +22,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "Thisisourlittlesecret.";
-
+const secret = process.env.SECRET;
 userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
